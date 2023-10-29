725252b498ddbc8bebcb51fccf107d68323ee8e3
Sai Ramya Linga <sairamyalinga@gmail.com>
2023-10-28 17:23:18 -0700

    Add logic to handle file upload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+var multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.use(cors());
@@ -13,7 +15,13 @@ app.get('/', function (req, res) {
 
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), function (req,res) {
 
+  const name = req.file.originalname;
+  const type = req.file.mimetype;
+  const size = req.file.size;
+  res.json({name,type,size})
+})
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
